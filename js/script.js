d60c1ed5875e54aa110d3450f81ecbdd3900c342
mart505 <67597744+mart505@users.noreply.github.com>
2020-06-29 19:17:04 +0200

    new

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var car = {
+	make: "VW",
+	type: "Polo",
+	color: "blue",
+	isTurnedOn: false,
+	numberOfWheels: 4,
+
+	turnOn: function () {
+		this.isTurnedOn = true;
+		return isTurnedOn;
+		
+	},
+	fly: function () {
+		alert("Wat leuk, je hebt op me gedrukt!");
+	}
+};
+
+var bankAccount = {
+	moneyCount: 0,
+};
+
+console.log("Welcome to the console!")
